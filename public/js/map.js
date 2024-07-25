090eb1012adb0454385a029aa4f9f9c002cf57b0
Vaibhav-cls <vaibhavghanekar19@gmail.com>
2024-07-25 21:47:31 +0530

    Integrated map using mapbox

---
+++ public/js/map.js
@@ -0,0 +1,8 @@
+import maplibregl from "maplibre-gl";
+import "maplibre-gl/dist/maplibre-gl.css";
+var map = new maplibregl.Map({
+  container: "map", // container id
+  style: "https://demotiles.maplibre.org/style.json", // style URL
+  center: [0, 0], // starting position [lng, lat]
+  zoom: 1, // starting zoom
+});
