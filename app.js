32dbd4c0db9a27c75d6ca82cc1d27ee31f997a80
Vaibhav-cls <vaibhavghanekar19@gmail.com>
2025-02-24 16:49:10 +0530

    Add filters and search functionality to listings page; redirect root to listings

--- app.js
+++ app.js
@@ -83,6 +83,9 @@ app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
+app.get("/", (req, res) => { 
+  res.redirect("/listings");
+});
 app.all("*", (req, res, next) => {
   next(new ExpressError(404, "Page not found"));
 });
